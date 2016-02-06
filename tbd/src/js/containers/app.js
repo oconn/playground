56f99c2956c890bfa35060e7bb533069735a4396
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2016-02-06 10:25:15 -0500

    add dropzone support

--- tbd/src/js/containers/app.js
+++ tbd/src/js/containers/app.js
@@ -1,11 +1,34 @@
 import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Dropzone from '../components/dropzone';
 
 class App extends React.Component {
+
+    renderDropzone() {
+        const options = {
+            autoProcessQueue: false,
+            uploadMultiple: true,
+            url: '/test'
+        };
+
+        const events = {
+
+        };
+
+        return (
+            <Dropzone
+                dropzoneOptions={options}
+                dropzoneEvents={events}
+            />
+        );
+    }
+
     render() {
         return <div>
             <h1>Initial Setup</h1>
+
+            {this.renderDropzone()}
         </div>
     }
 }
